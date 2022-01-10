24fbb2001b9d27f4d2bcf94d3c22805be0c61a06
Alan Tamez <alantamez28@hotmail.com>
2022-01-10 11:40:08 -0600

    Se agrego modal de más informacion de los pokemodes y cambio de diseño

--- src/index.js
+++ src/index.js
@@ -14,6 +14,14 @@ const search = (() => {
     const searchPokemon = async (id) => {
         try {
             const pokemon = await getPokemon(id)
+
+            const objPokemon = {
+                imagePokemon: pokemon.sprites.front_default,
+                pokemonName: pokemon.species.name,
+                atack: pokemon.stats[1].base_stat,
+                defence: pokemon.stats[2].base_stat
+            }
+
             setTimeout(() => {
 
                 Swal.fire({
@@ -23,9 +31,49 @@ const search = (() => {
                     showConfirmButton: false,
                     timer: 1500
                 })
-                imgPokemon.src = pokemon.sprites.front_default;
-                namePokemon.textContent = pokemon.species.name
+
+                imgPokemon.src = objPokemon.imagePokemon;
+                namePokemon.textContent = objPokemon.pokemonName;
+
             }, 1000)
+
+            setTimeout(() => {
+                Swal.fire({
+                    html: `
+                    <div class="box-img-modal">
+                        <img src="${objPokemon.imagePokemon}" alt="Square">
+                    </div>
+
+                    <section class="box-modal">
+
+                    <h1 id="modal_pokemon_name">${objPokemon.pokemonName}</h1>
+                    
+                    <div class="container">
+                    <div class="row align-items-center">
+
+                        <div class="col-6">
+                            <h4 class="text-left mt-3 class_decoration">Ataque</h4>
+                        </div>
+
+                        <div class="col">
+                            <h4 id="id_abilities" class="text-center mt-3 class_decoration">${objPokemon.atack}</h4>
+                        </div>
+
+                        <div class="col-6 mt-4">
+                            <h4 class="text-left class_decoration">Defensa</h4> <br>
+                        </div>
+
+                        <div class="col mt-4">
+                            <h4 class="text-center class_decoration">${objPokemon.defence}</h4> <br>
+                        </div> 
+                    </div>
+
+                    </div>
+                    
+                </section>`,
+                })
+
+            }, 2500)
 
         } catch (error) {
 
