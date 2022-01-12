9dbc6ede65b42f5f96354c10cf845230cdcfc0a3
Alan Tamez <alantamez28@hotmail.com>
2022-01-12 15:19:04 -0600

    se elimino comentarios en header.js

--- src/components/js/header.js
+++ src/components/js/header.js
@@ -1,6 +1,5 @@
 const header = (() =>{
     const header = document.querySelector("#navbar-header");
-
     header.innerHTML = `
     <nav class="navbar navbar-expand-lg navbar-dark bg-warning border border-dark  justify-content-center"
     style="padding: 8px;">
@@ -10,11 +9,6 @@ const header = (() =>{
                 height="60px">
             <span id="title_logo">¿Quién es ese pokémon?</span>
         </a>
-        <!-- <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
-            data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
-            aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button> -->
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                 <!-- <li class="nav-item">
